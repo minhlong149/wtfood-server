a175561a73c2e008c65b15faf0582ace940b576b
minhlong149 <minhlong9514@gmail.com>
2023-07-24 16:16:04 +0700

    Add count for dish's ingredients

--- services/dishes.go
+++ services/dishes.go
@@ -7,15 +7,28 @@ import (
 )
 
 type Dish struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Category string `json:"category"`
-	Image    string `json:"image"`
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Category    string `json:"category"`
+	Image       string `json:"image"`
+	Ingredients int    `json:"ingredients"`
 }
 
-func GetRandomDish() (dish Dish, err error)  {
-	row := config.Db.QueryRow(`SELECT * FROM dishes ORDER BY random() LIMIT 1`)
-	err = row.Scan(&dish.Id, &dish.Name, &dish.Category, &dish.Image)
+func GetRandomDish() (dish Dish, err error) {
+	row := config.Db.QueryRow(`
+		SELECT
+			d.id,
+			d.name,
+			d.category,
+			d.image,
+			COUNT(ingredient_id) as ingredients
+		FROM dishes d
+		JOIN recipes r ON r.dish_id = d.id
+		GROUP BY d.id
+		ORDER BY random() LIMIT 1
+	`)
+
+	err = row.Scan(&dish.Id, &dish.Name, &dish.Category, &dish.Image, &dish.Ingredients)
 
 	if err != nil {
 		log.Println(err)
